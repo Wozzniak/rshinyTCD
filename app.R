befa4b2446e7c35a4a5153006a6831bd52554f16
Wozzniak <patrick.vitielli@numericable.fr>
2025-03-17 14:53:05 +0100

    nineth

--- app.R
+++ app.R
@@ -150,3 +150,23 @@ ui <- dashboardPage(
                        app_colors = c("#F0811F", "#152969"),
                        app_linewidth = 3)
   ))
+
+
+server <- function(input, output, session) {
+  
+  
+  table_indicateur <- reactive({
+    df %>% 
+      dplyr::filter(Indicateur== stringr::str_extract(input$select, "^[0-9]+") & Croisement == stringr::str_remove(input$select, "^[0-9]+ - ")) %>% 
+      dplyr::select(where(~ any(!is.na(.)))) %>% 
+      dplyr::select(-Indicateur_Croisement)
+  }) 
+  
+  callModule(module = shinypivottabler,
+             id = "id",
+             show_title = FALSE,
+             theme = theme,
+             data = table_indicateur)
+}
+
+shinyApp(ui, server)
